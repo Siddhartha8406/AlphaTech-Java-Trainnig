aa9a0fe9af8c3a8757a2d8d8dcc24cacb3d5fea1
Siddhartha8406 <yerasi.venkat8406@gmail.com>
2023-03-04 14:12:36 +0530

    update: divisible

--- divisible.java
+++ divisible.java
@@ -2,11 +2,11 @@ class divisible {
     public static void main(String args[]){
         int i;
         for(i=1;i<=100;i++){
-            if(i%10==0){
-                System.out.println(i+" is divisible by 3");
+            if(i%7==0){
+                System.out.println(i+" is divisible by 7");
             }
             else{
-                System.out.println(i+" is not divisible by 3");
+                System.out.println(i+" is not divisible by 7");
             }
         }
     }
