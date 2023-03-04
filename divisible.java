ca502acaafa86bd274f1db641c0e97de7fcc3c92
Siddhartha8406 <yerasi.venkat8406@gmail.com>
2023-03-04 15:10:55 +0530

    update to 7

--- divisible.java
+++ divisible.java
@@ -2,7 +2,7 @@ class divisible {
     public static void main(String args[]){
         int i;
         for(i=1;i<=100;i++){
-            String des = i%3==0? (i+" is divisible by 3"):(i+ " is not divisible by 3");
+            String des = i%7==0? (i+" is divisible by 7"):(i+ " is not divisible by 7");
             System.out.println(des);
         }
     }
